f0a7ac9052574456023699622061c9bf21e38ff6
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-10 16:10:24 +0530

    Styled-component initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-
-const Header = styled.h1`
-  color: green;
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <Header>The Wild Oasis!</Header>
-      <Button onClick={() => alert("Check in")}>Check In</Button>
-      <Button onClick={() => alert("Check out")}>Check Out</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis!</Heading>
+            <div>
+              <Heading as="h2">Check in and Out</Heading>
+              <Button onClick={() => alert("Check in")}>Check In</Button>
+              <Button onClick={() => alert("Check out")}>Check Out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <div>
+              <Heading as="h3">Forms!</Heading>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
