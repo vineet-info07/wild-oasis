0b79c868e2d11de78ae163e79db73d42d423a366
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-10 16:53:36 +0530

    added routing into an application

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-import React from "react";
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis!</Heading>
-            <div>
-              <Heading as="h2">Check in and Out</Heading>
-              <Button onClick={() => alert("Check in")}>Check In</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check out")}
-              >
-                Check Out
-              </Button>
-            </div>
-          </Row>
-          <Row type="vertical">
-            <div>
-              <Heading as="h3">Forms!</Heading>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </div>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cabins() {
+  return <div>Cabins</div>;
+}
+
+export default Cabins;
