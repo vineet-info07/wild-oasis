6e0b01ef0b33ec60d8e94523ce9d7e29719654ca
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-11 16:41:49 +0530

    Building sideNav and mainNavigation

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -7,10 +9,18 @@ const StyledSidebar = styled.aside`
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/ -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
