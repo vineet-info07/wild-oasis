6e3136c166a14c0d1df4406f122a2449827b7af9
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-12 16:43:30 +0530

    Setting storage bucket

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -6,7 +6,12 @@ function Cabins() {
     getCabins().then((data) => console.log(data));
   }, []);
 
-  return <div>Cabins</div>;
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <img src="https://wvgmybxowixrniygtywn.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg" />
+    </div>
+  );
 }
 
 export default Cabins;
