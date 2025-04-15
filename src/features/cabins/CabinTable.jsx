cbb05646fc679814c74a2f1af455549508765f95
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-15 10:03:37 +0530

    display notification using react-hot-toast library

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -9,7 +9,7 @@ const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
   font-size: 1.4rem;
-  background-color: var() (--color-grey-0);
+  background-color: var(--color-grey-0);
   border-radius: 7px;
   overflow: hidden;
 `;
