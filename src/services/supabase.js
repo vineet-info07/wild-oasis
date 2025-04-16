40cdad8b17687c0b1923a1e69a67eeefd44b2532
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-16 15:10:31 +0530

    Upload image from UI also into Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://wvgmybxowixrniygtywn.supabase.co";
+export const supabaseUrl = "https://wvgmybxowixrniygtywn.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2Z215Ynhvd2l4cm5peWd0eXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzODIzOTMsImV4cCI6MjA1OTk1ODM5M30.FkUapyKAVwxrQcswr7GtUFEVXx81K0vp08Qcq2Lj9L4";
 const supabase = createClient(supabaseUrl, supabaseKey);
