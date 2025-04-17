8892b91adbc929bb2b67d2e70e9827f0834aa5f2
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-17 09:44:19 +0530

    Fetching an application settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <Row type="vertical">
+      <Heading as="h1">Update hotel settings</Heading>
+      <UpdateSettingsForm />
+    </Row>
+  );
 }
 
 export default Settings;
