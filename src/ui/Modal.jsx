50f1919f1f02d12e26e5bc90db6fe143982576f1
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-18 21:14:21 +0530

    detecting a click outside the modal also creted custom hook for same

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,8 +1,8 @@
+import { cloneElement, createContext, useState, useContext } from "react";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
-import { cloneElement, createContext, useState } from "react";
-import { useContext } from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -75,12 +75,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
