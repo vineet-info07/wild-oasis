4e9402dd381893a22eceead5b18e1aa1dae548d7
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-23 14:39:43 +0530

    API-side sorting:Sorting bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -15,13 +15,18 @@ function useBookings() {
       : { field: "status", value: filterValue };
   // { field: "totalPrice", value: 5000, method: "gte" };
 
+  // 2. SORT
+
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
