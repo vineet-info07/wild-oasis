ef033bd70a1630b5b7745eebbb6189588b805aa1
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-23 13:14:53 +0530

    API side filtering: filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,17 +1,28 @@
+import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 
 function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // 1. FILTER
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
-
-  console.log(bookings);
 
   return { isLoading, bookings, error };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice,cabins!bookings_cabinId_fkey(name), guests!bookings_guestId_fkey(fullName,email)"
     );
+
+  // FILTER
+  // if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
