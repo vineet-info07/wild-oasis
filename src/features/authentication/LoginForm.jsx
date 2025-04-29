1cc6ec69ae2217f645d66bb7c1dd1249aa67be88
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-29 15:10:04 +0530

    Authentication : User login with supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import { useLogin } from "./useLogin";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("vineet@example.com");
+  const [password, setPassword] = useState("Pass860060");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form type="regular" onSubmit={handleSubmit}>
@@ -18,6 +25,7 @@ function LoginForm() {
           id="email"
           // This makes this form better for password managers
           autoComplete="username"
+          disabled={isLoading}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -27,13 +35,14 @@ function LoginForm() {
           type="password"
           id="password"
           autoComplete="current-password"
+          disabled={isLoading}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large" variation="primary">
-          Login
+        <Button size="large" variation="primary" disabled={isLoading}>
+          {!isLoading ? "Log in " : <SpinnerMini />}
         </Button>
       </FormRowVertical>
     </Form>
