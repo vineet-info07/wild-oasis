8ace9dc6569f76e64eee053b824f18bc68ffd07c
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-04-30 07:16:56 +0530

    Building a signup form

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Heading from "../ui/Heading";
+import SignupForm from "../features/authentication/SignUpForm";
 
 function Users() {
-  return <div>Users</div>;
+  return (
+    <>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </>
+  );
 }
 
 export default Users;
