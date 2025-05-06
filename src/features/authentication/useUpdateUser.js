d9e93b5f893158d4ec017486d7eeaa92177d4820
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-06 15:11:25 +0530

    worked on small fix

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -6,9 +6,10 @@ function useUpdateUser() {
   const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User account successfully updated.");
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueriesData(["user", user]);
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) => {
       toast.error(err.message);
